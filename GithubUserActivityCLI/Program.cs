4095611d9a35878493f413143ef5658f36aa2d92
edward cabanatan <edward.cabanatan@gmail.com>
2026-01-28 00:19:26 +0800

    Add project files.

---
+++ GithubUserActivityCLI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Headers;
+
+//while(true)
+//{
+//    Console.WriteLine("Enter command: ");
+//    var input = Console.ReadLine();
+
+//    if(input == "exit")
+//    {
+//        Console.WriteLine("Exiting program...");
+//        break;
+//    }
+//}
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+client.DefaultRequestHeaders.Add("User-Agent", "Edward's GithubActivityCLI");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    var json = await client.GetStringAsync($"https://api.github.com/users/edward-solutions/events/public");
+    Console.WriteLine(json);
+}
+
+
